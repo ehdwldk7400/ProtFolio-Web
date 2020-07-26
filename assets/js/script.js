09fcdd44f78a9d75e12de7178420fcadd9c621e6
ehdwldk7400 <yjw5951@gmail.com>
2020-07-26 20:21:32 +0900

    0726 2차수정

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//   let ht = $(window).height();
+//   let delta = -1;
+//   console.log(ht);
+//   $("section").height(ht);
+//   console.log($("section"));
+//   $("section").on("mousewheel", function (event) {
+//     if (delta < 0) {
+//       let next = $(this).next().offset().top;
+//       $("html, body").stop().animate({ scrollTop: next }, 1000);
+//     }
+//   });
+// });
